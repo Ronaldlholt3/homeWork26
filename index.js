b995dd42fbdb93b6a9e6f55435a699a6b2b68e84
Ronaldlholt3 <76957909+Ronaldlholt3@users.noreply.github.com>
2021-01-27 02:32:13 -0500

    hw

---
+++ index.js
@@ -0,0 +1,47 @@
+
+var myPet = {
+    "name": "Duck",
+    "legs": 2,
+    "tails": 1,
+    "friends": []
+  };
+var myBackPack = {
+    "Brand": "Nike",
+    "books": 3,
+    "pens": 2,
+    "classes": []
+  };
+var myHouse = {
+    "street": "dreamrd",
+    "windows": 4,
+    "rooms": 2,
+    "people": []
+  };
+
+  var testObj = {
+    "Duck": "Dog",
+    "Cat": "Sheep",
+    "Pig": "Bird"
+  };
+
+  var duckValue = testObj.duck;     
+var dogValue = testObj.dog;
+var catValue = testObj.cat;
+
+var testObj = {
+    "my car": "gas",
+    "my car hood": "engine",
+    "the cluch": "transmission"
+  };
+
+  var carValue = testObj["gas"]; 
+var hoodValue = testObj['engine'];
+
+var meRon = {
+    "name": "Ron",
+    "legs": 2,
+    "bossness": 100,
+    "friends": ["theworld"]
+  };
+
+  meRon['say'] = "litt!"
